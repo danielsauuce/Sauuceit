6b62c1714066cac934935bb1dd06091a3df9cf98
Daniel <danieladeyd77@gmail.com>
2025-12-22 14:52:11 +0000

    cors and logger implemented

--- CRUD Hono/src/index.ts
+++ CRUD Hono/src/index.ts
@@ -1,6 +1,14 @@
 import { Hono } from "hono";
+import { initDatabase } from "./database/db";
+import { cors } from "hono/cors";
+import { logger } from "hono/logger";
 
 const app = new Hono();
+
+const db = initDatabase();
+
+app.use("*", cors());
+app.use("*", logger());
 
 app.get("/", (c) => {
   return c.text("Hello Hono!");
