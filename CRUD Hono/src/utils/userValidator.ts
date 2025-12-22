4419fe8e20abd19cf270596ec22c709f23456e6f
Daniel <danieladeyd77@gmail.com>
2025-12-22 19:18:22 +0000

    Delete CRUD Hono directory

--- CRUD Hono/src/utils/userValidator.ts
+++
@@ -1,7 +0,0 @@
-import { z } from "zod";
-
-export const registerSchema = z.object({
-  username: z.string().min(3).max(25),
-  password: z.string().min(6),
-  role: z.enum(["admin", "user"]).optional(),
-});
